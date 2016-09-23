2de24844b77e3a319fcaaa64e173d25586084830
Tim <tim.bourgault@gmail.com>
2016-09-23 16:44:48 -0700

    instantiate method that will allow the second test to pass.

--- src/WordFrequency.php
+++ src/WordFrequency.php
@@ -2,12 +2,18 @@
 
     class RepeatCounter
     {
+
         function CountRepeats($phraseInput, $keywordInput)
         {
             $repeatCounter = 0;
-            $repeatCounter++;
+            $phraseInputArray = explode(" ", $phraseInput);
+            foreach ($phraseInputArray as $word) {
+                $word = str_replace("\"", "", $word);
+                if ($keywordInput == $word) {
+                    $repeatCounter++;
+                }
+            }
             return $repeatCounter;
         }
     }
-
  ?>
